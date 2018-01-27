6906f864ab015724d457124e8999c75599d35a07
Thomas Fossaert <thomas.fossaert@epitech.eu>
2018-01-27 15:05:30 +0100

    [+] Adding small IA + raycast on player

---
+++ Assets/Script/IAFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAFollow : MonoBehaviour {
+
+	private GameObject wayPoint;
+	private Vector2 wayPointPos;
+
+	void Start () {
+		wayPoint = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () {
+		FollowToDeath ();
+	}
+
+	void FollowToDeath() {
+		wayPointPos = new Vector2(wayPoint.transform.position.x, wayPoint.transform.position.y);
+		transform.position = Vector3.MoveTowards(transform.position, wayPointPos, 10.0f * Time.deltaTime);
+	}
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerScript : MonoBehaviour {
+	
+	private Boolean Spotted;
+	public Transform sightStart;
+	public Transform sightEnd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+
+		if (Input.GetKeyDown ("space"))
+			Strike ();
+	}
+
+	void Strike() {
+		RaycastHit2D hit = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("Enemy"));
+		if (hit.collider != null) {
+			Debug.Log ("COLLIDING");
+		}
+	}
+}
