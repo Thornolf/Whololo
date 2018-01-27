e2830e8eb775ef74c35e2c766fe4a756bea6c706
Robin Grattepanche <robin.grattepanche@epitech.eu>
2018-01-28 00:25:34 +0100

    [+] Basic rooms gen + restart when arrive at exit

---
+++ Assets/Whololo/Script/Exit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    public float RestartDelay = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            Invoke("Restart", RestartDelay);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Whololo/Script/RoomGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGen : MonoBehaviour {
+
+    public GameObject[] availableRooms;
+    public GameObject current;
+
+    // Use this for initialization
+    void Start ()
+    {
+        int id = Random.Range(0, availableRooms.Length);
+        GameObject room = (GameObject)Instantiate(availableRooms[id]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
