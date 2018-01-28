cb180d6f95134c26a820490c853955ab25d36d5c
Robin Grattepanche <robin.grattepanche@epitech.eu>
2018-01-28 15:11:12 +0100

    [+] master

---
+++ Assets/Script/CharacterStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+	// Use this for initialization
+	public bool isAlive = true;
+	public int hitpoint = 100;
+	public float speed = 2.0f;
+	public float attackSpeed = 1.3f;
+
+	public Sprite deadSprite;
+	public SpriteRenderer spriteR;
+
+	void Start () {
+		spriteR = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAlive == false) {
+			spriteR.sprite = deadSprite;
+
+		}
+		if (hitpoint <= 0)
+			isAlive = false;
+	}
+}
---
+++ Assets/Script/IAShot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAShot : MonoBehaviour {
+
+	public GameObject projectile;
+	private CharacterStats stats;
+	private double nextAttackAllowed = 0.0f;
+	public ParticleSystem	deadParticle;
+	public ParticleSystem	convParticle;
+	private bool okPart = false;
+
+	// Use this for initialization
+	void Start () {
+		stats = GetComponent<CharacterStats> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance (transform.position, GameObject.FindGameObjectWithTag ("Player").transform.position);
+
+		if (stats.isAlive) {
+			if (dist < 10.5f) {
+				if (Time.time > nextAttackAllowed) {
+					nextAttackAllowed = Time.time + stats.attackSpeed;
+					CreateProjectile ();
+				}
+			}
+		} else {
+			gameObject.tag = "Untagged";
+			if (!okPart) {
+				ParticleSystem part = (ParticleSystem)Instantiate (convParticle);
+				part.transform.position = transform.position;
+				ParticleSystem partConv = (ParticleSystem)Instantiate (deadParticle);
+				partConv.transform.position = transform.position;
+				okPart = true;
+			}
+		}
+	}
+
+	void CreateProjectile()
+	{
+		Instantiate (projectile, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+	}
+}
